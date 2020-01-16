4d04eab808d27a22e6960c91f0da345cbca5e360
Bilal El Malki <bilal_el_malki@hotmail.com>
2020-01-15 23:49:28 -0500

    Little adjustements on tests (product service + integration)

--- assignment/src/test/java/com/centric/assignment/integration/IntegrationTest.java
+++ assignment/src/test/java/com/centric/assignment/integration/IntegrationTest.java
@@ -1,10 +1,25 @@
 package com.centric.assignment.integration;
 
+import com.centric.assignment.controller.model.command.CreateProductCommand;
+import com.centric.assignment.controller.model.view.ProductView;
 import com.centric.assignment.entity.Product;
+import com.centric.assignment.entity.Tag;
+import com.centric.assignment.service.IProductService;
+import com.centric.assignment.service.ProductService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
@@ -12,17 +27,52 @@ public class IntegrationTest {
     @Autowired
     WebTestClient webTestClient;
 
+    private IProductService iProductService;
 
+    private final List<Product> products = Arrays.asList(new Product(
+            UUID.randomUUID().toString(),
+            "name",
+            "description",
+            "brand",
+            Collections.emptyList(),
+            "apparel"
+            , LocalDateTime.now()
+    ), new Product(
+            UUID.randomUUID().toString(),
+            "name2",
+            "description2",
+            "brand2",
+            Collections.emptyList(),
+            "apparel"
+            , LocalDateTime.now()
+    ), new Product(
+            UUID.randomUUID().toString(),
+            "name3",
+            "description2",
+            "brand2",
+            Collections.emptyList(),
+            "apparel"
+            , LocalDateTime.now()
+    ));
+
+    @BeforeEach
+    public void init(){
+
+        iProductService = Mockito.mock(ProductService.class);
+
+    }
 
 
     @Test
     void shouldGetAllProduct(){
 
+        /*
+        This test should work fine if the database wasn't in memory
+         */
 
-
-        this.webTestClient
+        webTestClient
                 .get()
-                .uri("/v1/products")
+                .uri("/v1/products?category=apparel&page=0&limit=10")
                 .exchange()
                 .expectStatus()
                 .isOk()
@@ -34,10 +84,23 @@ public class IntegrationTest {
     @Test
     void shouldInsertProduct(){
 
-        this.webTestClient
+
+        /*
+        This test should work fine if the database wasn't in memory
+         */
+
+        webTestClient
                 .post()
                 .uri("/v1/products")
-                .body(new Product(), Product.class)
+                .bodyValue(new Product(
+                        UUID.randomUUID().toString(),
+                        "name",
+                        "description",
+                        "brand",
+                        Collections.emptyList(),
+                        "category"
+                        , LocalDateTime.now()
+                ))
                 .exchange()
                 .expectStatus()
                 .isOk();
--- assignment/src/test/java/com/centric/assignment/service/ProductServiceTest.java
+++ assignment/src/test/java/com/centric/assignment/service/ProductServiceTest.java
@@ -1,20 +1,55 @@
 package com.centric.assignment.service;
 
+import com.centric.assignment.controller.model.command.CreateProductCommand;
+import com.centric.assignment.entity.Product;
 import com.centric.assignment.repository.ProductRepository;
 import com.centric.assignment.response.BadRequestException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ProductServiceTest {
 
     private IProductService iProductService;
     private ProductRepository productRepository;
+    private final List<Product> products = Arrays.asList(new Product(
+            UUID.randomUUID().toString(),
+            "name",
+            "description",
+            "brand",
+            Collections.emptyList(),
+            "apparel"
+            , LocalDateTime.now()
+    ), new Product(
+            UUID.randomUUID().toString(),
+            "name2",
+            "description2",
+            "brand2",
+            Collections.emptyList(),
+            "apparel"
+            , LocalDateTime.now()
+    ), new Product(
+            UUID.randomUUID().toString(),
+            "name3",
+            "description2",
+            "brand2",
+            Collections.emptyList(),
+            "apparel"
+            , LocalDateTime.now()
+    ));
 
 
     @BeforeEach
@@ -38,4 +73,36 @@ class ProductServiceTest {
 
         assertThrows(BadRequestException.class, () -> iProductService.findAllPagedByDateCreated(null, Pageable.unpaged()));
     }
+
+    @Test
+    void shouldGetAllProducts() {
+
+        iProductService.findAllPagedByDateCreated("category", Pageable.unpaged());
+
+        //Verify that repository receives a findAllpagedByDateCreated request
+        Mockito.verify(productRepository, Mockito.times(1)).findAllPagesByCategory(Mockito.anyString(), Mockito.any());
+
+        //Database in memory so there's no data expected
+        assertEquals(0, iProductService.findAllPagedByDateCreated("apparel", PageRequest.of(0, 5, Sort.by(Sort.Direction.DESC,"createdAt"))).size());
+    }
+
+    @Test
+    void shouldInsertProduct(){
+
+        /*
+        Creating a createProductCommand to verify if the repository receives and saves a product
+         */
+        CreateProductCommand createProductCommand = new CreateProductCommand();
+        createProductCommand.setName("name");
+        createProductCommand.setBrand("brand");
+        createProductCommand.setCategory("category");
+        createProductCommand.setDescription("description");
+        createProductCommand.setTags(Collections.emptyList());
+
+        iProductService.insertProduct(createProductCommand);
+        Mockito.verify(productRepository, Mockito.times(1)).save(Mockito.any());
+
+    }
+
+
 }
